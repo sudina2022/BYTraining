377b17b92d4fa66344009a6dc843e2812698098b
sudina2022 <97794472+sudina2022@users.noreply.github.com>
2022-01-17 08:03:02 +0530

    Update PrimeNumberCheck.java

--- src/com/by/functioninterface/PrimeNumberCheck.java
+++ src/com/by/functioninterface/PrimeNumberCheck.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class PrimeNumberCheck{
-	public static void main(String s) {
+	public static void main(String[] s) {
 		 List<Integer> numbers = Arrays.asList( 2, 3, 4, 5, 6, 7, 8, 9, 20);
 		 List<Integer> finalList = numbers.stream()
 	                             .filter(PrimeNumberCheck::isPrime)
